3660c10785f232b1f0cd0f61dd12657d7f867301
JackGhL <xxt1205@gmail.com>
2022-05-06 00:55:40 +0900

    댓글 창 수정2차

--- src/main/java/comment/CommentDAO.java
+++ src/main/java/comment/CommentDAO.java
@@ -1,5 +1,32 @@
 package comment;
 
-public class CommentDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class CommentDAO {
+	public class BoardDAO {
+		
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
+		
+		public Connection getConnection() throws Exception{
+			
+			Context init=new InitialContext();
+			DataSource ds=(DataSource)init.lookup("java:comp/env/jdbc/MysqlDB");
+			Connection con=ds.getConnection();
+			return con;
+		}
+		public void closeDB() {
+			if(rs!=null) try { rs.close(); } catch (Exception e2) {	}
+			if(pstmt!=null) try { pstmt.close(); } catch (Exception e2) {	}
+			if(con!=null) try { con.close(); } catch (Exception e2) {	}	
+		}
+	}
 }
